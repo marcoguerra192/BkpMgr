3c58d4f7f12a3ec1638dbac062577fa4e19bf6cd
Marco Guerra <marco.guerra192@gmail.com>
2017-05-31 17:02:49 +0200

    Modificata Notevolmente la logica: Conf di base una una classe VirtualLocation che rappresenta solo un path, che potrebbe esistere o non esistere. Da VirtualLocation discende Location, classe in cui invece il path DEVE ESISTERE ED ESSERE ACCESSIBILE AL MOMENTO DELLA CREAZIONE. La classe Conf dispone di un metodo getWorkingConf che genera una configurazione ristretta ad una sotto lista di sources e dests in cui sia possibile creare delle Location derivate delle VirtualLocation. Vengono restituite anche le liste di destinazioni sbagliate.

--- Locations.py
+++ Locations.py
@@ -1,12 +1,58 @@
-## Classe Location definisce dove prendere e dove salvare i files
+# -*- coding: utf-8 -*-
 
-import os 
+# Classe VirtualLocation definisce un path virtuale che non esiste necessariamente
+# Classe Location DEVE AVERE UN PATH VALIDO
 
-class Location:
-	'Classe Location definisce dove prendere e dove salvare i files'
+import os
+import datetime
 
-	def __init__(self, locPath): # costruttore
-		try:                                        # verifica che il path sia valido, se non lo è chi lo chiama deve gestire 
-			self.path = os.path.abspath(locPath)    # ed eliminare l'oggetto non valido
-		except e:
-			raise e
+
+class VirtualLocation(object):
+    def __init__(self, locPath):  # costruttore
+        '''' Classe VirtualLocation definisce un'interfaccia per le sorgenti e destinazioni
+			 da cui copiare e su cui scrivere, controllando l'accessibilità, il mount eccetera
+
+			 Location(Path) deve ricevere un path valido per UNA DIRECTORY
+			 getPath ritorna path
+			 getLastSync ritorna l'ultima volta che l'oggetto è stato utilizzato
+			 touchDate(dt = now) riceve un datetime e lo salva come time di ultimo accesso (default now())
+		'''
+        self.path = os.path.abspath(locPath)
+        # nella location Virtuale NON CONTROLLO che esista già il path (HDD scollegato per esempio)
+        # => ACCETTA ANCHE PATH CHE NON ESISTONO!
+
+        self.read_ok = os.access(self.path, os.R_OK)  # verifica i privilegi lettura e scrittura
+        self.write_ok = os.access(self.path, os.W_OK)
+
+        self.last_sync = None
+
+    def checkRead(self):
+        return self.read_ok
+
+    def checkWrite(self):
+        return self.write_ok
+
+    def checkAll(self):
+        return self.read_ok and self.write_ok
+
+    def getPath(self):
+        return self.path
+
+    def getLastSync(self):
+        return self.last_sync
+
+    def touchDate(self, dt=datetime.datetime.now()):  # record that the obj was updated now
+        self.last_sync = dt
+
+class Location(VirtualLocation):
+
+    def __init__(self, locPath):
+
+        VirtualLocation.__init__(self,locPath)
+
+        if not os.path.isdir(self.path):
+            raise OSError # in questo caso il path DEVE ESSERE VALIDO ESISTENTE E MONTATO
+
+        self.read_ok = os.access(self.path, os.R_OK)  # verifica i privilegi lettura e scrittura
+        self.write_ok = os.access(self.path, os.W_OK)
+
